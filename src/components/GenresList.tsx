86171fb52cf17d0d93509e2bda3df7f51864f5f1
Mohammad-marghoob <marghoob.mohamad1998@gmail.com>
2025-02-02 11:59:16 +0330

    Fetch the genres

---
+++ src/components/GenresList.tsx
@@ -0,0 +1,13 @@
+import useGenres from "@/hooks/useGenres";
+
+export default function GenresList() {
+  const { genres, error, loading } = useGenres();
+
+  return (
+    <ul>
+      {genres.map((genre) => (
+        <li key={genre.id}>{genre.name}</li>
+      ))}
+    </ul>
+  );
+}
